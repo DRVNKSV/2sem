71ff110cbdd7e43fff97502c9a1e0a1dbaab8818
DRVNKSV <161410380+DRVNKSV@users.noreply.github.com>
2026-03-30 11:57:09 +0300

    добавила моды сс и дд

--- command.c
+++ command.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include "mem.h"
+#include "modes.h"
 typedef struct {
     word mask;
     word opcode;
     char * name;
     void (*do_command)(void);
 } Command;
+Arg ss;
+Arg dd;
 void do_halt();
 void do_move();
+void do_add();
 void do_nothing();
 void do_sob();
-void do_add();
 void do_inc();
 Command cmd[] = {
     {0170000, 0060000, "add", do_add},
@@ -19,15 +22,20 @@ Command cmd[] = {
     {0170000, 0005200, "inc", do_inc},
     {0170000, 0077000, "sob", do_sob},
     {0000000, 000000, "unknown", do_nothing},
-
 };
 void do_halt(){
-    trace("THE END");
+    printf("r0: %o, r1: %o, r2: %o, r3: %o,r4: %o, r5: %o, r6: %o, r7: %o", reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7]);
+    printf("THE END");
     exit(0);
 }
 void do_move(){
-
+    w_write(dd.adr, ss.val);
+}
+void do_add(){
+    w_write(dd.adr, ss.val + dd.val);
 }
 void do_nothing(){
 
 }
+
+
